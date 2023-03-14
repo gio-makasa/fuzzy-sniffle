433c08acf70f79dc6179f0003296ecc40b0f5d86
gio <giorgimakasarashvili@gmail.com>
2023-03-14 20:09:47 +0400

    portfolio

---
+++ static/js/variables.js
@@ -0,0 +1,4 @@
+export const devStack = document.getElementById('devStack');
+export const profilePhoto = document.getElementById('profilePhoto');
+export const aboutMe = document.getElementById('aboutMe');
+export const devName = document.getElementById('devName');
