87fc37e148b0018bb79096b237400858bb5da19e
gio <giorgimakasarashvili@gmail.com>
2023-07-16 17:39:12 +0400

    assigment 3: direvtives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username: string = '';
+  hidden = false;
 
-  onReset() {
-    this.username = '';
+  onC() {
+    this.hidden = !this.hidden;
   }
 }
